d994af6ea67e4adbdddebb51eba9b4aab6db82d9
Riccardo Muzzi <r.muzzi@reply.it>
2022-11-10 10:32:18 +0100

    format

--- vat-calc.js
+++ vat-calc.js
@@ -3,28 +3,26 @@
 const args = process.argv.slice(2);
 
 taxBrackets=[
-        {
-                mult: 0.23,
-                lowerBound: 0,
+	{
+		mult: 0.23,
+		lowerBound: 0,
 		upperBound: 15000
-        },
-        {
-                mult: 0.25,
+	},
+	{
+		mult: 0.25,
 		lowerBound: 15000,
-                upperBound: 28000
-        },
-        {
-                mult: 0.35,
+		upperBound: 28000
+	},
+	{
+		mult: 0.35,
 		lowerBound: 28000,
-                upperBound: 50000
-
-        },
-        {
-                mult: 0.43,
+		upperBound: 50000
+	},
+	{
+		mult: 0.43,
 		lowerBound: 50000,
-                upperBound: Infinity
-
-        }
+		upperBound: Infinity
+	}
 ];
 
 function calculateTaxes( income ){
@@ -41,7 +39,6 @@ function calculateTaxes( income ){
 	return taxes.reduce( (val1, val2) => {
 		return val1+val2;
 	}, 0);
-	
 }
 
 taxes = calculateTaxes( args[0] );
